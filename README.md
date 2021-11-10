421d15be67c00233ecedeec66455027cc4e36878
cm298 <cpanda595@gmail.com>
2021-11-10 21:23:23 +1300

    Updated Comments and README.md

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-This is a simple visualisation of the Collatz Conjecture Problem.
+# This is a simple visualisation of the Collatz Conjecture Problem.
 
-If it is an odd number: 
-    We multiple by 3 and add 1
+- If it is an odd number: 
+      We multiple by 3 and add 1
 
-If it is an even number: 
-    We divide by 2
+- If it is an even number: 
+      We divide by 2
 
-We keep applying these rules, untill we reach a loop of 4 -> 2 -> 1
+We keep applying these rules, until we reach a loop of 4 -> 2 -> 1
 
 Every positive number will end up in the 4,2,1 loop. 
+
+So far this is the result I have gotten:
+
+![Figure1](Figure_1.png)
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# If it is even we divide by 2
-# If it is odd we multiply it by 3 and add 1
-# Stop if we reach 1
-
+# For graphic
 import matplotlib.pyplot as plt
 
 # Max numbers we are willing to go to
@@ -14,29 +11,37 @@ xCords = []
 yCords = []
 
 
+# Function to calculate the steps
 def calculate(n):
+    # Number of times we loop
     num = 0
+    # The number we are working with
     temp = n
+    # While we don't reach the 4-2-1 loop
     while temp != 1:
         if (temp % 2) == 0:
-            # Even number 
+            # Even number, divide by 2
             temp = temp / 2
         else:
-            # Odd number
+            # Odd number, multiply by 3 and add 1
             temp = temp * 3 + 1
-        num += 1
+        num += 1  # Increment the number of loops
 
+    # Add the values to the lists
     xCords.append(n)
     yCords.append(num)
 
 
+# For each of the values in 1 - 9999
 for x in range(min_int, max_int):
+    # Calculate how many times it needs to be calculated
     calculate(x)
 
+# Display it into a scatter plot
 plt.scatter(xCords, yCords, c="blue")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Scatter Plot")
+plt.xlabel("# Being applied to the conjecture")
+plt.ylabel("How many #'s to reach loop")
+plt.title("Visualising Collatz Conjecture")
 
 # To show the plot
 plt.show()
